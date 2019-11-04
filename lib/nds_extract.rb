d43a483f30342171801364f1f167e9337cd7d965
Robert M Ricci <rmricci121@gmail.com>
2019-11-04 16:30:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -30,7 +30,7 @@ end
  def gross_for_director(director_data)
   movies_index = 0 
   worldwide_grosses ={}
-  binding.pry
+  #binding.pry
   while movies_index < director_data.length do 
     directors_total = director_data[:movies][movies_index][:worldwide_gross]
     worldwide_grosses[director_data] += directors_total
