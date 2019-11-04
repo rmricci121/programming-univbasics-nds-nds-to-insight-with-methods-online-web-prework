9a1a414ae5a1484195124cc10cf67a5f7f12600e
Robert M Ricci <rmricci121@gmail.com>
2019-11-04 15:39:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,12 +7,23 @@ require 'directors_database'
 pp directors_database
 
 def directors_totals(nds)
-  index = 0 
-  movies = directors_database[0][:movies]
-   while index < movies.length do
-     puts movies[index][:title]
-   index += 1
-   end
+  i directors_index = 0 
+  grand_total = {} 
+  while directors_index < nds.length do
+      directors_name = nds[directors_index][:name]
+      grand_total[directors_name] = 0  
+      movies = nds[directors_index][:movies]
+      movies_index = 0 
+    while movies_index < movies.length do
+  
+     grand_total[directors_name] += movies[movies_index][:worldwide_gross]
+      movies_index += 1
+    end
+    directors_index += 1
+  end
+
+  return grand_total
+end
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
