11bee3a937a09df63443fa5b6d31bfae7fb41339
Robert M Ricci <rmricci121@gmail.com>
2019-11-04 16:06:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,20 +29,19 @@ end
 # using director_data as input
  def gross_for_director(director_data)
   directors_index = 0 
-  grand_total = {} 
-
-   while directors_index < director_data.length do
+  worldwide_grosses = {} 
+    while directors_index < director_data.length do
        directors_name = director_data[directors_index][:name]
-       grand_total[directors_name] = 0  
+       worldwide_grosses[directors_name] = 0  
        movies = director_data[directors_index][:movies]
        movies_index = 0 
     while movies_index < movies.length do
-     grand_total[directors_name] += movies[movies_index][:worldwide_gross]
+     worldwide_gross[directors_name] += movies[movies_index][:worldwide_gross]
       movies_index += 1
     end
     directors_index += 1
   end
 
-  return grand_total
+  return worldwide_grosses
 
 end
